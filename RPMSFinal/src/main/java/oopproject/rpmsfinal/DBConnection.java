eb23c1cfa2dea5624ece65d4678345bfa3bc31e6
Ali Ahsan <136238207+aliahsan08@users.noreply.github.com>
2025-05-13 15:42:05 +0500

    Update DBConnection.java

--- RPMSFinal/src/main/java/oopproject/rpmsfinal/DBConnection.java
+++ RPMSFinal/src/main/java/oopproject/rpmsfinal/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // JDBC connection parameters
     private static final String URL  = "jdbc:mysql://127.0.0.1:3306/rpms?user=root";
     private static final String USER = "root";
-    private static final String PASS = "qwerty99+";
+    private static final String PASS = "your password";
 
     static {
         try {
